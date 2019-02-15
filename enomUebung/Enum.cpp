691c1e8947e4a0e3943fc88bbb1217090c06b503
Dennis K <golden_talon@hotmail.de>
2019-02-15 16:37:18 +0100

    Alterscheck und Variable aus funktion raus

--- enomUebung/Enum.cpp
+++ enomUebung/Enum.cpp
@@ -13,7 +13,7 @@ struct Attribute {
 	int	  HP;
 };
 
-std::string Ausgabe(int eingabe, Attribute werte)
+std::string Ausgabe(Attribute werte)
 {
 	if (werte.Typ == DIGIMON)
 	{
@@ -38,42 +38,57 @@ int main()
 	Attribute Digimon{ DIGIMON,90,200 };	// Startwerte
 	Attribute Pokemon{ POKEMON,3,50 };	// Startwerte
 	Attribute Qpie{ QPIE,9000, 1 };	// Startwerte
+	
+	int i{ 1 };
 
-	int eingabe{ 0 };
-	std::cout << "Gebe ein (1)Digimon (2)Pokemon (3)QPIE: ";
-	std::cin >> eingabe;
+	while (i == 1)
+	{
+		int eingabe{ 0 };
+		std::cout << "Gebe ein: (1)Digimon (2)Pokemon (3)QPIE: ";
+		std::cin >> eingabe;
+	
 
-	if (eingabe == 1)
-	{
-		std::cout << Ausgabe(eingabe, Digimon);
-		std::cout << "Gebe das Alter vom Digimon ein: ";
-		std::cin >> Digimon.Alter;
-		std::cout << "Gebe die HP vom Digimon ein: ";
-		std::cin >> Digimon.HP;
-		std::cout << "Dein Digimon ist " << Digimon.Alter << " Jahre alt und hat " << Digimon.HP << "HP.\n";
+		if (eingabe == 1)
+		{
+			std::cout << Ausgabe(Digimon);
+			std::cout << "Gebe das Alter vom Digimon ein: ";
+			std::cin >> Digimon.Alter;
+			std::cout << "Gebe die HP vom Digimon ein: ";
+			std::cin >> Digimon.HP;
+			if (Digimon.Alter == 1)
+			{
+				std::cout << "Dein Digimon ist " << Digimon.Alter << " Jahr alt und hat " << Digimon.HP << "HP.\n";
+			}
+			else
+			{
+				std::cout << "Dein Digimon ist " << Digimon.Alter << " Jahre alt und hat " << Digimon.HP << "HP.\n";
+			}
+			i++;
+		}
+		else if (eingabe == 2)
+		{
+			std::cout << Ausgabe(Pokemon);
+			std::cout << "Gebe das Alter vom Pokemon ein: ";
+			std::cin >> Pokemon.Alter;
+			std::cout << "Gebe die HP vom Pokemon ein: ";
+			std::cin >> Pokemon.HP;
+			std::cout << "Dein Pokemon ist " << Pokemon.Alter << " Jahre alt und hat " << Pokemon.HP << "HP.\n";
+			i++;
+		}
+		else if (eingabe == 3)
+		{
+			std::cout << Ausgabe(Qpie);
+			std::cout << "Gebe das Alter vom Qpie ein: ";
+			std::cin >> Qpie.Alter;
+			std::cout << "Gebe die HP vom Qpie ein: ";
+			std::cin >> Qpie.HP;
+			std::cout << "Dein Qpiee ist " << Qpie.Alter << " Jahre alt und hat " << Qpie.HP << "HP.\n";
+			i++;
+		}
+		else
+		{
+			std::cout << "Falsche Eingabe! Gebe 1,2 oder 3 ein!\n";
+		}
 	}
-	else if (eingabe == 2)
-	{
-		std::cout << Ausgabe(eingabe, Pokemon);
-		std::cout << "Gebe das Alter vom Pokemon ein: ";
-		std::cin >> Pokemon.Alter;
-		std::cout << "Gebe die HP vom Pokemon ein: ";
-		std::cin >> Pokemon.HP;
-		std::cout << "Dein Pokemon ist " << Pokemon.Alter << " Jahre alt und hat " << Pokemon.HP << "HP.\n";
-	}
-	else if (eingabe == 3)
-	{
-		std::cout << Ausgabe(eingabe, Qpie);
-		std::cout << "Gebe das Alter vom Qpie ein: ";
-		std::cin >> Qpie.Alter;
-		std::cout << "Gebe die HP vom Qpie ein: ";
-		std::cin >> Qpie.HP;
-		std::cout << "Dein Qpiee ist " << Qpie.Alter << " Jahre alt und hat " << Qpie.HP << "HP.\n";
-	}
-	else
-	{
-		std::cout << "1,2 oder 3 habe ich gesagt!\n";
-	}
-
 	return 0;
 }
