52311081862adadd795842816901f0452837f278
Dennis K <golden_talon@hotmail.de>
2019-02-15 14:16:19 +0100

    Final work!

--- enomUebung/Enum.cpp
+++ enomUebung/Enum.cpp
@@ -9,21 +9,21 @@ enum Namen {
 
 struct Attribute {
 	Namen Typ;
-	int Alter;
-	int HP;
+	int	  Alter;
+	int	  HP;
 };
 
 std::string Ausgabe(int eingabe, Attribute werte)
 {
-	if (werte.Typ == Namen::DIGIMON)
+	if (werte.Typ == DIGIMON)
 	{
 		return "Ein Digimon wurde gewaehlt!\n";
 	}
-	else if (werte.Typ == Namen::POKEMON)
+	else if (werte.Typ == POKEMON)
 	{
 		return "Ein Pokemon wurde gewaehlt!\n";
 	}
-	else if (werte.Typ == Namen::QPIE)
+	else if (werte.Typ == QPIE)
 	{
 		return "QPIE wurde gewaehlt!\n";
 	}
@@ -35,9 +35,9 @@ std::string Ausgabe(int eingabe, Attribute werte)
 
 int main()
 {
-	Attribute Digimon{ Namen::DIGIMON,90,200 };
-	Attribute Pokemon{ Namen::POKEMON,3,50 };
-	Attribute Qpie{ Namen::QPIE,9000, 1 };
+	Attribute Digimon{ DIGIMON,90,200 };	// Startwerte
+	Attribute Pokemon{ POKEMON,3,50 };	// Startwerte
+	Attribute Qpie{ QPIE,9000, 1 };	// Startwerte
 
 	int eingabe{ 0 };
 	std::cout << "Gebe ein (1)Digimon (2)Pokemon (3)QPIE: ";
@@ -46,17 +46,29 @@ int main()
 	if (eingabe == 1)
 	{
 		std::cout << Ausgabe(eingabe, Digimon);
-		std::cout << "Es hat die Werte :" << Digimon.Alter << " Jahre alt. " << Digimon.HP << " HP.\n";
+		std::cout << "Gebe das Alter vom Digimon ein: ";
+		std::cin >> Digimon.Alter;
+		std::cout << "Gebe die HP vom Digimon ein: ";
+		std::cin >> Digimon.HP;
+		std::cout << "Dein Digimon ist " << Digimon.Alter << " Jahre alt und hat " << Digimon.HP << "HP.\n";
 	}
 	else if (eingabe == 2)
 	{
 		std::cout << Ausgabe(eingabe, Pokemon);
-		std::cout << "Es hat die Werte :" << Pokemon.Alter << " Jahre alt. " << Pokemon.HP << " HP.\n";
+		std::cout << "Gebe das Alter vom Pokemon ein: ";
+		std::cin >> Pokemon.Alter;
+		std::cout << "Gebe die HP vom Pokemon ein: ";
+		std::cin >> Pokemon.HP;
+		std::cout << "Dein Pokemon ist " << Pokemon.Alter << " Jahre alt und hat " << Pokemon.HP << "HP.\n";
 	}
 	else if (eingabe == 3)
 	{
 		std::cout << Ausgabe(eingabe, Qpie);
-		std::cout << "Es hat die Werte :" << Qpie.Alter << " Jahre alt. " << Qpie.HP << " HP.\n";
+		std::cout << "Gebe das Alter vom Qpie ein: ";
+		std::cin >> Qpie.Alter;
+		std::cout << "Gebe die HP vom Qpie ein: ";
+		std::cin >> Qpie.HP;
+		std::cout << "Dein Qpiee ist " << Qpie.Alter << " Jahre alt und hat " << Qpie.HP << "HP.\n";
 	}
 	else
 	{
